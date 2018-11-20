a39d83137f3dd49ade0d54408209b6b7a07fde34
LUJO <joseph.lu@cargosmart.com>
2018-11-20 17:16:17 +0800

    update font css

---
+++ src/main/java/com/forum/global/GlobalCode.groovy
@@ -0,0 +1,10 @@
+package com.forum.global
+
+class GlobalCode {
+    public final static String LOGIN_VERIFY_OK = '200'
+    public final static String LOGIN_CODE_OK = '201'
+
+    public final static String LOGIN_VERIFY_FAIL = '300'
+    public final static String LOGIN_CODE_FAIL = '301'
+    public final static String LOGIN_CODE_FREQUENT = '302'
+}
