5eeb934aad77db8724a9cefa24936467bdaff3b9
LUJO <joseph.lu@cargosmart.com>
2018-11-22 08:45:29 +0800

    update font css

--- src/main/java/com/forum/His.java
+++ src/main/java/com/forum/His.java
@@ -1,8 +1,18 @@
 package com.forum;
 
+import com.forum.model.dto.LoginInfo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class His {
-
+    @Autowired
+    private LoginInfo loginInfo;
+    @GetMapping("r")
+    public LoginInfo f(){
+        loginInfo.setLoginCode("!23");
+        loginInfo.setPassword("123");
+        return loginInfo;
+    }
 }
